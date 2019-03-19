7d08d8b0bb9f96d1cce3e7e6851ad1b69635a7dc
elkindm <44894517+elkindm@users.noreply.github.com>
2019-03-18 21:44:50 -0500

    Create head.php
    se crea planilla encabezado

---
+++ application/views/encabezado/head.php
@@ -0,0 +1,21 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?><!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Empresa Grupo bien pensado</title>
+
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	 <script src="http://localhost/Gpbp/bootstrap/js/jquery-3.3.1.js"></script>
+	<link rel="stylesheet" href="http://localhost/Gpbp/bootstrap/css/bootstrap.css">
+	
+	<script src="http://localhost/Gpbp/bootstrap/js/bootstrap.js"></script>
+	<script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
+	
+</head>
+<body>
+<div class="jumbotron">
+  <h1>Empresa</h1> 
+  
+</div>
