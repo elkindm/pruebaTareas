b787cbb6ac5e313d31d25b10f0b4b374a95fd1a5
elkindm <44894517+elkindm@users.noreply.github.com>
2019-03-20 21:34:35 -0500

    Update inicio.php
    se crea login

--- application/views/contenido/inicio.php
+++ application/views/contenido/inicio.php
@@ -1,1 +1,48 @@
-<button class="btn btn-success">Login</button>
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?><!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Prueba</title>
+
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	 <script src="http://localhost/pruebaTareas/bootstrap/js/jquery-3.3.1.js"></script>
+	<link rel="stylesheet" href="http://localhost/pruebaTareas/bootstrap/css/bootstrap.css">
+	
+	<script src="http://localhost/pruebaTareas/bootstrap/js/bootstrap.js"></script>
+	<script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
+	
+</head>
+<body>
+<div class="jumbotron">
+  <h1>Prueba App Tareas</h1> 
+  
+</div>
+<div class="container" align="center">
+
+	<div class="card text-white bg-secondary mb-3" style="max-width: 18rem;">
+	  <div class="card-header">Iniciar Sessión</div>
+	  <div class="card-body">
+	    
+		<table>
+			<tr>
+				<th>Usuario</th>
+			</tr>
+			<tr>
+				<td><input type="text" name="usuario" class="form-control"></td>
+			</tr>
+			<tr>
+				<th>Contraseña</th>
+			</tr>
+			<tr>
+				<td><input type="password" name="pass" class="form-control"></td>
+			</tr>
+			<br>
+			<tr>
+				<td><br><button class="btn btn-warning btn-lg btn-block">Ingresar</button></td>
+			</tr>
+		</table>
+	  </div>
+	</div>
+</div>
