1259685d51de8bc677c0da66238b7e68cf48f07d
elkindm <44894517+elkindm@users.noreply.github.com>
2019-03-20 21:33:31 -0500

    Update head.php
    se actualiza librerias

--- application/views/encabezado/head.php
+++ application/views/encabezado/head.php
@@ -4,18 +4,34 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 <html lang="en">
 <head>
 	<meta charset="utf-8">
-	<title>Empresa Grupo bien pensado</title>
+	<title>Prueba</title>
 
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	 <script src="http://localhost/Gpbp/bootstrap/js/jquery-3.3.1.js"></script>
-	<link rel="stylesheet" href="http://localhost/Gpbp/bootstrap/css/bootstrap.css">
+	 <script src="http://localhost/pruebaTareas/bootstrap/js/jquery-3.3.1.js"></script>
+	<link rel="stylesheet" href="http://localhost/pruebaTareas/bootstrap/css/bootstrap.css">
 	
-	<script src="http://localhost/Gpbp/bootstrap/js/bootstrap.js"></script>
+	<script src="http://localhost/pruebaTareas/bootstrap/js/bootstrap.js"></script>
 	<script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
 	
 </head>
 <body>
-<div class="jumbotron">
-  <h1>Empresa</h1> 
-  
-</div>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="#">Prueba</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="navbar-nav">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Usuarios</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Tareas</a>
+      </li>
+     
+    </ul>
+  </div>
+</nav>
