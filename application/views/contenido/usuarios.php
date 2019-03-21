98c20353ef9d3d3839855277439525e1821df6f4
elkindm <44894517+elkindm@users.noreply.github.com>
2019-03-20 22:10:35 -0500

    Update usuarios.php

--- application/views/contenido/usuarios.php
+++ application/views/contenido/usuarios.php
@@ -47,7 +47,7 @@
     <div class="modal-content">
     	<form id="guardaUsuario" method="post">
       	  <div class="modal-header">
-	        <h5 class="modal-title" id="exampleModalLabel">Crear Usuario</h5>
+	        <h5 class="modal-title" id="exampleModalLabel">Formulario de  Usuario</h5>
 	        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
 	          <span aria-hidden="true">&times;</span>
 	        </button>
