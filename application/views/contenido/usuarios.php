13482db07081a478511acc232874771f3ef47328
elkindm <44894517+elkindm@users.noreply.github.com>
2019-03-20 21:33:52 -0500

    Create usuarios.php
    se crea vista usuarios

---
+++ application/views/contenido/usuarios.php
@@ -0,0 +1,41 @@
+<div class="container">
+	<br>
+	<nav aria-label="breadcrumb">
+	  <ol class="breadcrumb">
+	    <li class="breadcrumb-item active" aria-current="page">Configuración de Usuarios</li>
+	  </ol>
+	</nav>
+	<div class="card text-center">
+	 
+	  <ul class="nav nav-tabs" role="tablist">
+		  <li class="nav-item">
+		    <a class="nav-link active" data-toggle="tab" href="#home">Usuarios</a>
+		  </li>
+		  <li class="nav-item">
+		    <a class="nav-link" data-toggle="tab" href="#menu1">Perfiles</a>
+		  </li>
+		  
+		</ul>
+	  <div class="card-body">
+
+	  	<div class="tab-content">
+	      <div class="tab-pane container active" id="home">
+	      	<table class="table table-striped ">
+	      		<thead class="thead-dark">
+	      			<tr>
+		      			<th>Usuario</th>
+		      			<th>Nombres</th>
+		      			<th>Estado</th>
+		      			<th>Perfil</th>
+		      		</tr>
+	      		</thead>
+	      		
+	      	</table>
+	      </div>
+		  <div class="tab-pane container fade" id="menu1">Perfiles...
+		  </div>
+		  
+		</div>
+	  </div>
+	</div>
+</div>
