f1160bfc3a20d7b2c705bfdc594d1eba80c5ae8d
elkindm <44894517+elkindm@users.noreply.github.com>
2019-03-20 23:17:17 -0500

    Update Usuarios.php

--- application/controllers/Usuarios.php
+++ application/controllers/Usuarios.php
@@ -7,7 +7,7 @@ class Usuarios extends CI_Controller {
 	 {
 		 parent::__construct();
 		 $this->load->model('modelos');
-		 $this->load->model('usuarios');
+		 $this->load->model('usuario');
 	 }
 	public function index()
 	{
@@ -17,7 +17,53 @@ class Usuarios extends CI_Controller {
 	}
 	public function guardaUsuario($value='')
 	{
-		return "respuesta afirmativa";
+		$usuario = $this->input->post('usuario'); 
+		$pass = $this->input->post('pass'); 
+		$cpass = $this->input->post('cpass'); 
+		$identificacion = $this->input->post('identificacion'); 
+		$pnom = $this->input->post('pnom'); 
+		$snom = $this->input->post('snom'); 
+		$pape = $this->input->post('pape'); 
+		$sape = $this->input->post('sape'); 
+		$estado = $this->input->post('estado');
+		$feha=Date('Y-m-d'); 
+		//consultar si exite el usuario
+			$res= $this->usuario->find("usuario='$usuario'");
+			if (count($res)>0) {
+				//actualiza
+				$tabla="usuario";
+				$data = array(
+				 
+				 'clave' => $pass,
+				 'confirmaClave' => $cpass,
+				 'numeroIdentificacion' => $identificacion,
+				 'primerNombre' => $pnom,
+				 'segundoNombre' => $snom,
+				 'primerApellido' => $pape,
+				 'segundoApellido' => $sape,
+				 'estado' => $estado,
+				 'fechaEstado' => $feha,
+				);
+				$respuesta = $this->modelos->actualiza($tabla, $data,$usuario);
+			}else{
+				//crea registro
+				$tabla="usuario";
+				$data = array(
+				 'usuario' => $usuario,
+				 'clave' => $pass,
+				 'confirmaClave' => $cpass,
+				 'numeroIdentificacion' => $identificacion,
+				 'primerNombre' => $pnom,
+				 'segundoNombre' => $snom,
+				 'primerApellido' => $pape,
+				 'segundoApellido' => $sape,
+				 'estado' => $estado,
+				 'fechaEstado' => $feha,
+				);
+			
+			$respuesta = $this->usuario->guardar($tabla, $data );
+			}
+		echo json_encode($respuesta);
 	}
 }
 ?>
